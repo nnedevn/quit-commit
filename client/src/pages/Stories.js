baf088d5858f6f866583f5cd066f81d6442f59b1
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-29 21:27:47 -0800

    initial Reddit API call

--- client/src/pages/Stories.js
+++ client/src/pages/Stories.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+const Articles = (props) => {
+  const options = props.results.map((item)=>(
+    <li>
+      <h2>{item.data.title}</h2>
+      <a>{item.data.url}</a>
+      <p>{item.data.selftext}</p>
+    </li>
+  ))
+  return <ul>{options}</ul>
+}
 
 class Stories extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  getInfo = () => {
+  axios.get(`https://www.reddit.com/r/stopsmoking/new.json?sort=new`)
+    .then(({data})=>{
+      this.setState({
+        articles: data.data.children
+      })
+    })
+  }
+
   render(){
+
     return(
       <div>
-        <h2>Stories page</h2>
+        <h1>Stories From the StopSmoking Sub-Reddit</h1>
+        <Articles results={this.state.articles} />
       </div>
     );
   }
