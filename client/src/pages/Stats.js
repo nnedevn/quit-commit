6fe58d8ff1d57604712d0ebf617df7da51d1a8d7
Nick Nedev <nedevn@gmail.com>
2018-01-30 20:43:10 -0800

    Adds tick fx to Stats Compoent

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react';
 
 class Stats extends Component {
-  render(){
-    if(this.props.user && this.props.user.name){
+  constructor() {
+    super();
+    this.state = {
+      quitDateEpoch: null,
+      rightNowEpoch:'',
+      days: '',
+      hours: '',
+      mins:'',
+      seconds: ''
+    }
+  }
+
+  tick = () =>{
+    let msecPerMin = 60 * 1000;
+    let msecPerHour = msecPerMin * 60;
+    let msecPerDay = 24 * msecPerHour;
+
+    let currentDateEpoch = new Date().getTime();
+    let interval = currentDateEpoch - this.state.quitDateEpoch;
+  
+    let days = Math.floor(interval / msecPerDay);
+    interval = interval - days * msecPerDay;
+    let hours = Math.floor(interval / msecPerHour);
+    interval = interval - hours * msecPerHour;
+    let mins = Math.floor(interval / msecPerMin);
+    interval -= mins * msecPerMin;
+    let seconds = Math.floor(interval / 1000);
+
+    console.log('days generated:', days);
+    this.setState({
+      rightNowEpoch: currentDateEpoch,
+      days: days,
+      hours: hours,
+      mins:mins,
+      seconds: seconds
+    });
+
+
+    console.log('days from state: ', this.state.days);
+  }
+
+  componentWillMount(){
+    let quitDateToEpoch = new Date(this.props.user.quitDate).getTime();
+    this.setState({
+      quitDateEpoch: quitDateToEpoch
+    })
+  }
+  componentDidMount(){
+    this.timerId = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount(){
+    // TODO: Stop the timer just in case;
+  }
+  
+  render() {
+    if (this.props.user && this.props.user.name) {
       return (<div>
-          <h2>HELLO AGAIN {this.props.user.name}!</h2>
-          <h4>Your email is {this.props.user.email}</h4>
-          <h4>You've been smoking for {this.props.user.yearsSmoked} years</h4>
-          <h4>You've smoked  {this.props.user.cigsPerDay} cigs per day</h4>
-          <h1>Stats page</h1>
-        </div>);
+
+
+
+        <h2>HELLO AGAIN {this.props.user.name}!</h2>
+        <div>{this.state.quitDateEpoch}</div>
+        <h4>You have been smoke free for {this.state.days} days, {this.state.hours} hours, {this.state.mins} minutes and {this.state.seconds} seconds.</h4>
+        <h4>You've been smoking for {this.props.user.yearsSmoked} years</h4>
+        <h4>You've smoked  {this.props.user.cigsPerDay} cigs per day</h4>
+        <h1>Stats page</h1>
+      </div>);
     }
     else {
       return (<p>Stats page</p>);
