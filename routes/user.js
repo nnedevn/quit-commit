4c6a3bbac0b051b24c07a3d048941ed239e47d13
Nick Nedev <nedevn@gmail.com>
2018-01-31 21:28:16 -0800

    Formats code in user.js route

--- routes/user.js
+++ routes/user.js
@@ -3,23 +3,28 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/user');
 
-router.put('/journal/new', function(req,res){
-console.log(req.body);
-  User.update({"_id": req.body.userId},
-    {$push:{"journalEntries":{ 
-      "moodRating": req.body.moodRating,
-      "journalEntry": req.body.journalEntry,
-      "createdOn": new Date()
-    } }},
-    function(err, numAffected){
-      if(err){console.log(err)
-      } else{
+router.put('/journal/new', function (req, res) {
+  console.log(req.body);
+  User.update({ "_id": req.body.userId },
+    {
+      $push: {
+        "journalEntries": {
+          "moodRating": req.body.moodRating,
+          "journalEntry": req.body.journalEntry,
+          "createdOn": new Date()
+        }
+      }
+    },
+    function (err, numAffected) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send()
         console.log(numAffected);
       }
     }
-)
+  )
 
 });
-
 
 module.exports = router;
