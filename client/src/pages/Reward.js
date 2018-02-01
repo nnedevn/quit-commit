04cf1fcd58e7aac47a54cfc9b2770f3b6bf35b55
Orion Weldon <orion@Orions-MacBook-Pro.local>
2018-02-01 11:36:56 -0800

    starting to build form on rewards page

--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -11,7 +11,16 @@ class Reward extends Component {
         <div className="treat-title">
           <h1>Treat Yo Self</h1>
         </div>
+        <div className="reward-desc">
+          <h2>What do you want to do with all that cash you are saving? </h2>
+          <h4>Throw a picture up here to remind yourself what youre working towards!</h4>
+        </div>
         <Upload />
+        <div>
+          <h3>How Much Does It Cost?</h3>
+          <span className="dollasign">$</span><input type="integer"></input>
+          <h3>Heres How Long It Will take to Save Up</h3>
+        </div>
       </div>
     );
   }
