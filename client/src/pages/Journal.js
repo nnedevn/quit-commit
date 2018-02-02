4aa9425126ade26122c5b92b065dfa5645edce0b
Nick_edu <nick_edu@nameless.local>
2018-02-02 14:40:29 -0800

    Fixes Journal COmponent update

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import JournalSubmitForm from './JournalSubmitForm';
 import JournalEntry from './JournalEntry';
+import { isArray } from 'util';
 class Journal extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       showForm: false,
@@ -11,19 +12,18 @@ class Journal extends Component {
     }
   }
 
-  returnToJournalOnSubmit = (entry) => {  
+  returnToJournalOnSubmit = (entry) => {
     console.log(entry);
     this.setState({
       showForm: false,
-      journalEntries: this.state.journalEntries.push(entry)
+      journalEntries: this.state.journalEntries.concat(entry)
     });
   }
 
-  componentWillMount(){
-    // console.log(this.props.user.journalEntries);
+  componentWillMount() {
     let journalEntriesWeird = this.props.user.journalEntries;
     let journalEntries = [];
-    for (let index in journalEntriesWeird){
+    for (let index in journalEntriesWeird) {
       journalEntries.push(journalEntriesWeird[index]);
     }
     this.setState({
@@ -33,13 +33,15 @@ class Journal extends Component {
 
   content = () => {
 
-    if (this.state.showForm){
-      return <JournalSubmitForm user={this.props.user} returnToJournalOnSubmit={this.returnToJournalOnSubmit}/>
+    if (this.state.showForm) {
+      return <JournalSubmitForm user={this.props.user} returnToJournalOnSubmit={this.returnToJournalOnSubmit} />
     } else {
-      return this.state.journalEntries.reverse().map(function(entry){
-        return <JournalEntry entry={entry}/>
+      console.log("######STUFF",isArray(this.state.journalEntries) );
+      let thing = this.state.journalEntries.reverse().map(function (entry) {
+        return <JournalEntry entry={entry} />
       });
- 
+      return thing;
+
     }
   }
 
@@ -47,16 +49,15 @@ class Journal extends Component {
     this.setState({
       showForm: !this.state.showForm
     })
-    this.state.showForm ? this.setState({buttonValue:"Show"}) : this.setState({buttonValue:"Hide"});
+    this.state.showForm ? this.setState({ buttonValue: "Show" }) : this.setState({ buttonValue: "Hide" });
   }
-  
-  render() {
 
+  render() {
 
     return (
       <div>
         {this.content()}
-        <input type="button" value={this.state.buttonValue} onClick={this.toggleForm}/>
+        <input type="button" value={this.state.buttonValue} onClick={this.toggleForm} />
         {this.journalContent}
       </div>
     );
