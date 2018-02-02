5fa14a2b50de19f0e9be16da5ce21c21f0639c4e
Nick_edu <nick_edu@nameless.local>
2018-02-02 12:23:24 -0800

    Fixes typo

--- client/src/pages/JournalEntry.js
+++ client/src/pages/JournalEntry.js
@@ -4,7 +4,7 @@ class JournalEntry extends Component{
   render(){
     return(
       <div className="journal-entry"> 
-        <h4>Mood rating: {this.props.entry.moodRaiting}</h4>
+        <h4>Mood rating: {this.props.entry.moodRating}</h4>
         <h5>Journal Entry: </h5>
         <p>{this.props.entry.journalEntry}</p> 
       </div>
